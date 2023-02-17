2e474584a4ebdf2cbbdab5e44f2f2094c533b458
Daryl_Moh <Daryl.Moh@hotmail.com>
2023-02-17 13:24:18 +0800

    fixed driver issue with the code

--- src/main/java/ibf2022/paf/day29/Day29Application.java
+++ src/main/java/ibf2022/paf/day29/Day29Application.java
@@ -1,13 +1,17 @@
 package ibf2022.paf.day29;
 
+import java.util.List;
+
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import ibf2022.paf.day29.services.TvShowService;
 
 @SpringBootApplication
-public class Day29Application {
+public class Day29Application implements CommandLineRunner {
 
 	@Autowired
 	private TvShowService tvShowService;
@@ -17,7 +21,11 @@ public class Day29Application {
 	
 	@Override
 	public void run(String... args){
+		//List<Document> results = tvshowSvc.countGenres();
+		List<Document> results = tvShowService.histogramOfRatings();
 
+		for (Document d: results)
+			System.out.printf(">>> %s\n", d.toJson());
 		
 	}
 
--- src/main/java/ibf2022/paf/day29/services/TvShowService.java
+++ src/main/java/ibf2022/paf/day29/services/TvShowService.java
@@ -8,10 +8,13 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.core.aggregation.BucketOperation;
 import org.springframework.data.mongodb.core.aggregation.GroupOperation;
 import org.springframework.data.mongodb.core.aggregation.SortOperation;
 import org.springframework.data.mongodb.core.aggregation.UnwindOperation;
 import org.springframework.stereotype.Service;
+
+import static ibf2022.paf.day29.Constants.*;
 
 @Service
 public class TvShowService {
@@ -20,17 +23,40 @@ public class TvShowService {
     private MongoTemplate template;
 
     public List<Document> countGenres(){
+
         // $unwind
         UnwindOperation unwindGenres = Aggregation.unwind(FIELD_GENRES);
 
         // $group
-        GroupOperation groupShowsByGenres = Aggregation.group(FIELD_GENRES);
+        GroupOperation groupShowsByGenres = Aggregation.group(FIELD_GENRES)
+            .count().as(FIELD_TOTAL);
 
         // $sort
         SortOperation sortByGenres = Aggregation.sort(Sort.by(Direction.ASC, FIELD_UNDERSCORE_ID));
+
+        Aggregation pipeline = Aggregation.newAggregation(unwindGenres, groupShowsByGenres, sortByGenres);
+
+        return template.aggregate(pipeline, COLLECTION_TV, Document.class)
+            .getMappedResults();
     }
 
     public List<Document> histogramOfRatings() {
+
+        // Create the required operation from the mongodb query
+
+        BucketOperation ratingsBucket = Aggregation.bucket(FIELD_RATING_AVERAGE)
+            .withBoundaries(3, 6, 9)
+            .withDefaultBucket(">9")
+            .andOutput(FIELD_NAME).push().as(FIELD_TITLES);
+
+        // Create the pipeline to hold the operation 
+
+        Aggregation pipeline = Aggregation.newAggregation(ratingsBucket);
+
+        // Use the template to get back the result by passing in the pipeline, collection and class type
+        
+        return template.aggregate(pipeline, COLLECTION_TV, Document.class)
+            .getMappedResults();
         
     }
 
